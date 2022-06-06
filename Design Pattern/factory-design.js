222fb313005f03cfbdfa6fa6e95aaa4c4dffed79
Amankumar Sadhwani <amankumar_sadhwani@epam.com>
2022-06-06 10:38:17 +0530

    added factory design pattern

---
+++ Design Pattern/factory-design.js
@@ -0,0 +1,34 @@
+const employees = [];
+function say() {
+  console.log("Hi, I am " + this.name + " and I am a " + this.type);
+}
+function Developer(name) {
+  this.name = name;
+  this.type = "Developer";
+}
+
+function Tester(name) {
+  this.name = name;
+  this.type = "Tester";
+}
+
+function EmployeeFactory() {
+  this.create = (name, type) => {
+    switch (type) {
+      case 1:
+        return new Developer(name);
+      case 2:
+        return new Tester(name);
+    }
+  };
+}
+
+const employeeFactory = new EmployeeFactory();
+
+employees.push(employeeFactory.create("Aman", 1));
+employees.push(employeeFactory.create("Rahul", 2));
+employees.push(employeeFactory.create("Rafael", 1));
+
+employees.forEach((emp) => {
+  say.call(emp);
+});
